c219f1d5453e75480f11c79fa1de9845d99553e2
Vincent Youmssi <vincentyoumssi@gmail.com>
2026-07-01 20:53:50 +0100

    feat(credit-case): édition des informations du dossier
    Ajoute un bouton « Modifier » dans l'en-tête du détail du dossier ouvrant
    une boîte de dialogue pré-remplie (client, type de produit, devise) qui
    appelle PUT /credit-cases/{id}. Le formulaire se réinitialise à l'ouverture
    et l'enregistrement reste désactivé tant qu'aucune modification n'est faite ;
    le cache du détail et la liste sont rafraîchis au succès.
    
    Refs: NIMBA-12

--- components/modules/credit-case/index.ts
+++ components/modules/credit-case/index.ts
@@ -1,6 +1,7 @@
 export { CreditCaseDashboard } from "./credit-case-dashboard";
 export { CreditCaseDetail } from "./credit-case-detail";
 export { CreateCaseDialog } from "./create-case-dialog";
+export { EditCaseDialog } from "./edit-case-dialog";
 export { CreditCaseList } from "./credit-case-list";
-export { useCreditCases, useCreditCase, useCreateCreditCase, creditCaseKeys } from "./useCreditCase";
+export { useCreditCases, useCreditCase, useCreateCreditCase, useUpdateCreditCase, creditCaseKeys } from "./useCreditCase";
 export type { CreditCase, CreditCaseSummary, CreditCaseStatus, ProductType, CreateCaseInput } from "./schema";
